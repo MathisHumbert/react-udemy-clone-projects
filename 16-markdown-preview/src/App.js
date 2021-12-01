86b3e060c84c51aaee874736cf567ee6c3b39fc9
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-01 11:16:39 +0100

    redid react project

---
+++ 16-markdown-preview/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+function App() {
+  const [value, setValue] = useState(`## markdown preview`);
+
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={value}
+          onChange={() => setValue(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{value}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default App;
