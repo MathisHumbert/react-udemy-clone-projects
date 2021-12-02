213267ccd11d88c68c5c00daaa26e5454af028e3
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-02 16:36:38 +0100

    redid react project

--- 18-pagination/src/App.js
+++ 18-pagination/src/App.js
@@ -3,13 +3,28 @@ import { useFetch } from './useFetch';
 import Follower from './Follower';
 function App() {
   const { loading, data } = useFetch();
-  const [index, setIndex] = useState(0);
+  const [page, setPage] = useState(0);
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
     if (loading) return;
-    setFollowers(data[index]);
+    setFollowers(data[page]);
   }, [loading]);
+
+  const prevClick = () => {
+    setPage((oldValue) => {
+      let newValue = oldValue - 1;
+      if (newValue < 0) return data.length - 1;
+      return newValue;
+    });
+  };
+  const nextClick = () => {
+    setPage((oldValue) => {
+      let newValue = oldValue + 1;
+      if (newValue > data.length - 1) return 0;
+      return newValue;
+    });
+  };
 
   return (
     <main>
@@ -23,7 +38,24 @@ function App() {
             return <Follower {...item} key={item.id} />;
           })}
         </div>
-        <div className="btn-container"></div>
+        <div className="btn-container">
+          <button className="prev" onClick={prevClick}>
+            prev
+          </button>
+          {data.map((item, index) => {
+            return (
+              <button
+                className={`page-btn ${index === page ? 'active' : null}`}
+                onClick={() => setPage(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+          <button className="next" onClick={nextClick}>
+            next
+          </button>
+        </div>
       </section>
     </main>
   );
