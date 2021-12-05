b2c321f952ca2f1fede71a421b950a7e7526b6cd
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-05 10:26:39 +0100

    working on react project

--- 19-stock-photos/src/App.js
+++ 19-stock-photos/src/App.js
@@ -1,12 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [photos, setPhotos] = useState([]);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos(photos);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(`${mainUrl}${clientID}`);
+  }, []);
+
+  return (
+    <>
+      <section className="search">
+        <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            placeholder="search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="form-input"
+          ></input>
+          <button type="submit" className="submit-btn">
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((item) => {
+            console.log(item);
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
